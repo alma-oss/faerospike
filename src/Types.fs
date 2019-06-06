b601ad6d12e75d858def6518e830237a83820c42
Petr Chromec <petr.chromec@lmc.eu>
2019-06-06 13:40:44 +0200

    Add toString function to configurations

--- src/Types.fs
+++ src/Types.fs
@@ -5,7 +5,15 @@ type ConnectionConfiguration = {
     Port: int
 }
 
+module ConnectionConfiguration =
+    let toString configuration =
+        sprintf "%s:%i" configuration.Host configuration.Port
+
 type Configuration = {
     Namespace: string
     SetName: string
 }
+
+module Configuration =
+    let toString configuration =
+        sprintf "%s/%s" configuration.Namespace configuration.SetName
